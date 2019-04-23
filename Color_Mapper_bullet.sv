5eac971a6588a6ec784878d4db3b3ae23beb1b68
usridas <36054325+usridas@users.noreply.github.com>
2019-04-23 00:03:47 -0500

    Put wall stuff

--- Color_Mapper_bullet.sv
+++ Color_Mapper_bullet.sv
@@ -14,10 +14,13 @@
 //-------------------------------------------------------------------------
 
 // color_mapper: Decide which color to be output to VGA for each pixel.
-module  color_mapper (  input			is_tank1, is_tank2, is_shooting1, is_shooting2, hit1,
+module  color_mapper (  input			is_tank1, is_tank2, is_shooting1, is_shooting2, is_bullet,
+												input 		is_wall1, is_wall2, is_wall3, is_wall4,
+												input 		[1:0] hit1, hit2,
 											  input			[2:0] tank_dir1, tank_dir2,
 				                input     [9:0] DrawX, DrawY,       // Current pixel coordinates
 											  input			[9:0] tankX1, tankX2, tankY1, tankY2, bullet_X1, bullet_Y1,
+												input 		[9:0] wall_X1, wall_X2, wall_X3, wall_X4, wall_Y1, wall_Y2, wall_Y3, wall_Y4,
 											  input			Clk,
                        	output logic [7:0] VGA_R, VGA_G, VGA_B // VGA RGB output
                      );
@@ -27,7 +30,7 @@ module  color_mapper (  input			is_tank1, is_tank2, is_shooting1, is_shooting2, 
 	 parameter [9:0] BackWidth = 10'd640;
 
     logic [7:0] Red, Green, Blue;
-	 logic [18:0] tank_addr, back_addr;
+	 logic [18:0] tank_addr, back_addr, bullet_addr;
 	 logic [23:0] RGB_tanku, RGB_tankr, RGB_tankl, RGB_tankd, RGB_back;
 
     // Output colors to VGA
@@ -51,7 +54,13 @@ module  color_mapper (  input			is_tank1, is_tank2, is_shooting1, is_shooting2, 
 //									  .data_Out(RGB_back)
 //									  );
 		frameRAM_Bullet bullet(.read_address(bullet_addr), .Clk(Clk),
- 									.data_Out(RGB_bullet)
+ 									.data_Out(RGB_bullet));
+
+		frameRAM_Wall_H horizon (.read_address(wall_addr), .Clk(Clk),
+ 									.data_Out(RGB_wall_h));
+
+		frameRAM_Wall_H verizon (.read_address(wall_addr), .Clk(Clk),
+ 									.data_Out(RGB_wall_v));
 
     always_comb
     begin
@@ -165,11 +174,39 @@ module  color_mapper (  input			is_tank1, is_tank2, is_shooting1, is_shooting2, 
 			endcase
 		end
 		else if (is_bullet) begin
-			bullet_addr = (DrawX - bullet_X1) + ((DrawY - bullet_Y1) << 3'd5);
-			if (RGB_bullet != 24'hFF0000) begin
+			bullet_addr = (DrawX - bullet_X1) + ((DrawY - bullet_Y1) << 3'd4);
+			if (RGB_bullet != 24'hFFFFFF) begin
 				Red = RGB_bullet & 24'hFF0000 >> 5'd16;
 				Green = RGB_bullet & 24'h00FF00 >> 4'd8;
 				Blue = RGB_bullet & 24'h0000FF;
+			end
+		else if (is_wall1) begin
+			hor_wall_addr = (DrawX - wall_X1) + ((DrawY - wall_Y1) << 3'd4);
+			if (RGB_wall_h != 24'hFFFFFF) begin
+				Red = RGB_wall_h & 24'hFF0000 >> 5'd16;
+				Green = RGB_wall_h & 24'h00FF00 >> 4'd8;
+				Blue = RGB_wall_h & 24'h0000FF;
+			end
+		else if (is_wall2) begin
+			wall_addr = (DrawX - wall_X2) + ((DrawY - wall_Y2) << 3'd4);
+			if (RGB_wall_v != 24'hFFFFFF) begin
+				Red = RGB_wall_v & 24'hFF0000 >> 5'd16;
+				Green = RGB_wall_v & 24'h00FF00 >> 4'd8;
+				Blue = RGB_wall_v & 24'h0000FF;
+			end
+		else if (is_wall3) begin
+			wall_addr = (DrawX - wall_X3) + ((DrawY - wall_Y3) << 3'd4);
+			if (RGB_wall_h != 24'hFFFFFF) begin
+				Red = RGB_wall_h & 24'hFF0000 >> 5'd16;
+				Green = RGB_wall_h & 24'h00FF00 >> 4'd8;
+				Blue = RGB_wall_h & 24'h0000FF;
+			end
+		else if (is_wall4) begin
+			wall_addr = (DrawX - wall_X4) + ((DrawY - wall_Y4) << 3'd4);
+			if (RGB_wall_v != 24'hFFFFFF) begin
+				Red = RGB_wall_v & 24'hFF0000 >> 5'd16;
+				Green = RGB_wall_v & 24'h00FF00 >> 4'd8;
+				Blue = RGB_wall_v & 24'h0000FF;
 			end
 	end
 
