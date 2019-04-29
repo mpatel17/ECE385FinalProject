a350e7281a85a938f6814ac7da4e4d5c5220cd6a
usridas <36054325+usridas@users.noreply.github.com>
2019-04-29 16:08:53 -0500

    random walls?

--- wall.sv
+++ wall.sv
@@ -1,34 +1,50 @@
 module  wall (	input 	Clk,                // 50 MHz clock
 								Reset,              // Active-high reset signal
-								frame_clk,          // The clock indicating a new frame (~60Hz)
 					input [9:0]   DrawX, DrawY, // Current pixel coordinates
-//					input 	num_walls,		//Tells how many walls should be set up
-					output  is_wall1, is_wall2, is_wall3, is_wall4, is_any_wall,     // Whether current pixel belongs to wall
+					input [9:0]		SW,
+					output  is_wall1, is_wall2, is_wall3, is_wall4,    // Whether current pixel belongs to wall
 					output [9:0] X1, X2, X3, X4, Y1, Y2, Y3, Y4
 					);
 
-		parameter [9:0] X_Min = 10'd0;       // Leftmost point on the X axis
-	   parameter [9:0] X_Max = 10'd639;     // Rightmost point on the X axis
-	   parameter [9:0] Y_Min = 10'd0;       // Topmost point on the Y axis
-	   parameter [9:0] Y_Max = 10'd479;     // Bottommost point on the Y axis
 		parameter [9:0] Vert_Width = 10'd32;
 		parameter [9:0] Vert_Height = 10'd64;
 		parameter [9:0] Hor_Width = 10'd64;
 		parameter [9:0] Hor_Height = 10'd32;
 
-		logic [9:0] counter;
-		
-		assign is_any_wall = is_wall1 || is_wall2 || is_wall3 || is_wall4;
+		wall_location wallX1(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b0), .location(SW), .location2(X1)
+												);
+		wall_location wallY1(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b1), .location(SW), .location2(Y1)
+												);
+		wall_location wallX2(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b0), .location(X1), .location2(X2)
+												);
+		wall_location wallY2(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b1), .location(Y1), .location2(Y2)
+												);
+		wall_location wallX3(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b0), .location(X2), .location2(X3)
+												);
+		wall_location wallY3(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b1), .location(Y2), .location2(Y3)
+												);
+		wall_location wallX4(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b0), .location(X3), .location2(X4)
+												);
+		wall_location wallY4(.Clk(Clk), .Reset(Reset),
+												.Wall(1'b1), .location(Y3), .location2(Y4)
+												);
 
-		assign X1 = 10'd10;
-		assign Y1 = 10'd20;
-		assign X2 = 10'd400;
-		assign Y2 = 10'd200;
-		assign X3 = 10'd320;
-		assign Y3 = 10'd240;
-		assign X4 = 10'd600;
-		assign Y4 = 10'd400;
-				
+		// assign X1 = 10'd10;
+		// assign Y1 = 10'd20;
+		// assign X2 = 10'd400;
+		// assign Y2 = 10'd200;
+		// assign X3 = 10'd320;
+		// assign Y3 = 10'd240;
+		// assign X4 = 10'd600;
+		// assign Y4 = 10'd400;
+
 		//Distance for each wall and horizontal and vertical dimensions
 		int DistX1, DistX2, DistX3, DistX4, DistY1, DistY2, DistY3, DistY4, Wh, Hh, Wv, Hv;
 	   assign DistX1 = DrawX - X1;
