fce099cc65a99cd8ca5ced286ba3c02d35944908
usridas <36054325+usridas@users.noreply.github.com>
2019-04-30 18:24:37 -0500

    Create Ram_Eight.sv

---
+++ Ram_Eight.sv
@@ -0,0 +1,35 @@
+/*
+ * ECE385-HelperTools/PNG-To-Txt
+ * Author: Rishi Thakkar
+ *
+ */
+
+module  frameRAM_eight
+(
+		input [18:0] read_address,
+		input Clk,
+
+		output logic [23:0] data_Out
+);
+
+logic [23:0] tank_palette [11:0];
+logic index;
+
+// mem has width of 1 bit and a total of 1024 addresses
+logic mem [0:1023];
+
+assign clk_palette[0] = 24'hFFFFFF;
+assign clk_palette[1] = 24'h000000;
+
+initial
+begin
+	 $readmemh("sprite_bytes/Eight_Sprite.txt", mem);
+end
+
+assign index = mem[read_address];
+
+always_ff @ (posedge Clk) begin
+	data_Out <= clk_palette[index];
+end
+
+endmodule
